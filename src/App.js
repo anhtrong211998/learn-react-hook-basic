a68b4fb3e88834a17e5c39d6f156d03f86c51adb
anhtrong211998 <anhtrong211998@gmail.com>
2022-01-24 15:56:38 +0700

    example about dynamic data in template

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = 'Eric'; //string
+  let number = 2021;//number 
+  let obj = { name: 'ERic', channel: 'Hoi Dan IT' };//object
+  let link = 'https://www.youtube.com/watch?v=frVHVZDpsp4&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E';//strig
   return (
     <div className="App">
+      {console.log('check obj: ', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT</h1>
+        <h1>Hello world with React and {name}!</h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank"  >Visit my channel</a>
       </header>
     </div>
   );
