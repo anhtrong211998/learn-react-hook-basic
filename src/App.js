36449cd619ea37cc52a0767df35ad8514d4967bb
anhtrong211998 <anhtrong211998@gmail.com>
2022-01-25 11:18:19 +0700

    example use spread operator and array.map

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     //// state in function compoennt 
     const [name, setName] = useState('HoiDanIT');
     const [address,setAddress] = useState('');
+    const [todos, setTodos] = useState([
+      { id: 'todo1', title: 'Watching Hoi Dan IT Channel' },
+      { id: 'todo2', title: 'Doing homework' },
+      { id: 'todo3', title: 'Playing game' }
+    ]);
 
     //// handle event
     const handleOnChange = (event) => {
@@ -14,7 +19,17 @@ function App() {
     }
 
     const handleOnClick = (event) =>{
-      setName(address);
+      //setName(address);
+      event.preventDefault();
+      if (!address) {
+        alert('emtpy input')
+        return;
+      }
+      //hook not merge state
+      //...spread syntax array js
+      let newTodo = { id: 'abc', title: address }
+      setTodos([...todos, newTodo])
+      setAddress('')
     }
     //re-render
   
@@ -24,6 +39,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and {name}!</h1>
+
+        <div className="todos-container" style={{textAlign:'left'}}>
+          {todos.map(todo => {
+            return (
+              <li className="todo-child" key={todo.id}> {todo.title}</li>
+            )
+          })}
+        </div>
+
         <input type="text" value={address} onChange={(event) => setAddress(event.target.value)} />
         <button type="button" onClick={(event) => handleOnClick(event)}>Click me</button>
       </header>
