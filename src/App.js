33d93a66c1e1e03d7e7d2ce51695998192ab6778
anhtrong211998 <anhtrong211998@gmail.com>
2022-01-25 10:15:18 +0700

    example use useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
 function App() {
-  let name = 'Eric'; //string
-  let number = 2021;//number 
-  let obj = { name: 'ERic', channel: 'Hoi Dan IT' };//object
-  let link = 'https://www.youtube.com/watch?v=frVHVZDpsp4&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E';//strig
+    //// state in function compoennt 
+    const [name, setName] = useState('HoiDanIT');
+    const [address,setAddress] = useState('');
+
+    //// handle event
+    const handleOnChange = (event) => {
+      setAddress(event.target.value);
+    }
+
+    const handleOnClick = (event) =>{
+      setName(address);
+    }
+    //re-render
+  
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and {name}!</h1>
-        <p>{JSON.stringify(obj)}</p>
-        <a href={link} target="_blank"  >Visit my channel</a>
+        <input type="text" value={address} onChange={(event) => setAddress(event.target.value)} />
+        <button type="button" onClick={(event) => handleOnClick(event)}>Click me</button>
       </header>
     </div>
   );
